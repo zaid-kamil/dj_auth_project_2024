a7d20ac40dd98f2b9cc96bf055b5cb03c631fa9b
zaid kamil <xaidmetamorphos@gmail.com>
2024-08-07 14:00:53 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+def home_view(request):
+    return render(request, 'home.html')
