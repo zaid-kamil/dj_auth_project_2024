c9dde23baddcb5a4320e8a945fcaee120c8886ce
zaid kamil <xaidmetamorphos@gmail.com>
2024-08-09 14:01:46 +0530

    authentication complete

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login', views.login_view, name='login'),
     path('register', views.register_view, name='register'),
+    path('logout', views.logout_view, name='logout'),
     path('', views.home_view, name='home'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
     return render(request, 'login.html')
 
 def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+        if password == cpassword:
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            return redirect('login')
     return render(request, 'register.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
 
 def home_view(request):
     return render(request, 'home.html')
